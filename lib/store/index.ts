4d23d0744e2677d880ce4457fe777a1e02ae22ba
Felipe Santos <felipeicloud11@gmail.com>
2025-10-28 21:30:37 -0300

    feat: export brand kit store from store module barrel
    Exports:
    - useBrandKitStore: Zustand hook
    - Type exports: BrandKitStore, BrandKitStoreState, BrandKitStoreActions, TemplateType
    
    Simplifies imports: import { useBrandKitStore } from '@/lib/store'

--- lib/store/index.ts
+++ lib/store/index.ts
@@ -6,3 +6,12 @@ export type {
   PresentationStoreActions,
   StoredPresentation,
 } from './presentation-store';
+
+// Brand Kit Store
+export { useBrandKitStore } from './brand-kit-store';
+export type {
+  BrandKitStore,
+  BrandKitStoreState,
+  BrandKitStoreActions,
+  TemplateType,
+} from './brand-kit-store';
