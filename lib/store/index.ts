e0ca18504ccbf6f2f642bedffd65eaaacf444f85
Felipe Santos <felipeicloud11@gmail.com>
2025-10-28 21:18:05 -0300

    feat: create barrel export for store module
    Exports:
    - usePresentationStore: Zustand hook
    - createPresentationFromResponse: helper function
    - Type exports: PresentationStore, PresentationStoreState,
      PresentationStoreActions, StoredPresentation
    
    Simplifies imports: import { usePresentationStore } from '@/lib/store'

---
+++ lib/store/index.ts
@@ -0,0 +1,8 @@
+// Presentation Store
+export { usePresentationStore, createPresentationFromResponse } from './presentation-store';
+export type {
+  PresentationStore,
+  PresentationStoreState,
+  PresentationStoreActions,
+  StoredPresentation,
+} from './presentation-store';
